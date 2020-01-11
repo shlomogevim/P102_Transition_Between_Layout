418ea4c1435e926429b1914d49fcfe426ae84b39
Shlomo <shlomo.gevim@gmail.com>
2020-01-11 22:16:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/p102_transition_between_layout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.p102_transition_between_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
